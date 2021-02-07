0cff9ecd6ef73fd631027d19d759e11c3772ad5c
lijiajun <673240713@qq.com>
2021-02-07 13:33:32 +0800

    6

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login'
+import Home from '../views/Home'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,10 @@ const routes = [
     path: '/login',
     component: Login
   },
+  {
+    path:"/home",
+    component:Home
+  }
   // {
   //   path: '/about',
   //   name: 'About',
