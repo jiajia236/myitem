1fe452015b9d1f7727b9ba0be4e5a96a7dca0ddb
lijiajun <673240713@qq.com>
2021-02-07 13:03:50 +0800

    disici

---
+++ src/views/Login.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="login">
+    <mt-header title="用户登录">
+      <router-link to="/" slot="left"><mt-button icon="back"></mt-button></router-link>
+      <router-link to="/reg" slot="right">去登陆</router-link>
+    </mt-header>
+  </div>
+</template>
