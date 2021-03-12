18e52f4f8223a3c6c4cead6d5262aa5015208d24
lijiajun <673240713@qq.com>
2021-03-12 19:48:21 +0800

    mm

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,15 @@ module.exports={
   chainWebpack:config=>{
     config.plugins.delete("prefetch")
   },
-  // devServer:{
-  //   proxy:{
-  //     '/api':{
-  //       target:'http://127.0.0.1:3000',
-  //       changeOrigin:true,
-  //       pathRewrite:{
-  //         '^/api':''
-  //       }
-  //     }
-  //   }
-  // }
+  devServer:{
+    proxy:{
+      '/api':{
+        target:'http://127.0.0.1:3000',
+        changeOrigin:true,
+        pathRewrite:{
+          '^/api':''
+        }
+      }
+    }
+  }
 }
